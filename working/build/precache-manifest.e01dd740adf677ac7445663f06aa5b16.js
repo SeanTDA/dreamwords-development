8e4c823890a1440e88da81f9138aac76bff33859
Sean Simon <ssimon@altvfx.local>
2022-06-16 14:39:39 +1000

    + build dir

---
+++ working/build/precache-manifest.e01dd740adf677ac7445663f06aa5b16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5357049cdfbda047b937",
+    "url": "/static/css/main.6cca227c.chunk.css"
+  },
+  {
+    "revision": "5357049cdfbda047b937",
+    "url": "/static/js/main.5357049c.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "0ae3bdba19ebe713a465",
+    "url": "/static/js/2.0ae3bdba.chunk.js"
+  },
+  {
+    "revision": "d9ce6d317115bc41b4f8",
+    "url": "/static/js/runtime~main.d9ce6d31.js"
+  },
+  {
+    "revision": "9fb2f43934d80cb95f511592b95e8cf1",
+    "url": "/index.html"
+  }
+];
