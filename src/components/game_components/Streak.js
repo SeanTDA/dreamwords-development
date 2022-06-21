eaf084689f8cbc708c23155b8a20ef543aa1d977
Sean Simon <ssimon@altvfx.local>
2022-06-21 16:34:45 +1000

    update

--- src/components/game_components/Streak.js
+++ src/components/game_components/Streak.js
@@ -5,41 +5,27 @@ import { AppContext } from "../../App";
 function Streak({ streakType }) {
 
     const appContext = useContext(AppContext);
-    const { streak, superStreak, superStreakHighScore } = appContext;
+    const { streak } = appContext;
 
 
     //if (streak <= 0) return;
     let displayStreak = streak;
-    let displaySuperStreak = superStreak;
-    let displaySuperStreakHighScore = superStreakHighScore;
+
 
 
     if (displayStreak <= 0) displayStreak = 0;
-    if (displaySuperStreak <= 0) displaySuperStreak = 0;
 
-    let superStreakClassName = displaySuperStreak > 0 ? "superStreak" : "superStreakInvisible";
+
+
 
 
     const streakClassName = displayStreak > 0 ? "aliveStreak" : "aliveStreak aliveStreakDull";
 
 
-    // High score display 
-    superStreakClassName += " superStreakHighScore";
-
-    const superStreakClassName01 = superStreakClassName + "-01";
-    const superStreakClassName02 = superStreakClassName + "-02";
-    const superStreakClassName03 = superStreakClassName + "-03";
-    const superStreakClassName04 = superStreakClassName + "-04";
-    const superStreakClassName05 = superStreakClassName + "-05";
-    const superStreakClassName06 = superStreakClassName + "-06";
-    const superStreakClassName07 = superStreakClassName + "-07";
-    const superStreakClassName08 = superStreakClassName + "-08";
-    const superStreakClassName09 = superStreakClassName + "-09";
-    const superStreakClassName10 = superStreakClassName + "-10";
 
     return (
                 <div className={streakClassName}>
-                    🔥 streak : {displayStreak}
+                    🔥 : {displayStreak}
                 </div>
     );
 }
