e1e7f94c50f8ae934c95c7386783ef9d6e014b56
Sean Simon <ssimon@altvfx.local>
2022-06-25 16:40:00 +1000

    + minoro changes

--- src/components/game_components/ImageClue.js
+++ src/components/game_components/ImageClue.js
@@ -7,15 +7,15 @@ import ImageCrop from './imageCrop';
 function ImageClue () {
 
     const appContext = useContext(AppContext);
-    const {levelData} = appContext;
+    const {levelData, pressedLetters } = appContext;
     
     return (
         <div className='image-clue'>
-            <Slider autoplay={true} dots={true} arrows={false} pauseOnFocus={true} autoplaySpeed={4200}>
-                <ImageCrop index={0} imageUrl={levelData.imageURL} />
-                <ImageCrop index={1} imageUrl={levelData.imageURL} />
-                <ImageCrop index={2} imageUrl={levelData.imageURL} />
-                <ImageCrop index={3} imageUrl={levelData.imageURL} />
+            <Slider autoplay={true} dots={true} arrows={false} pauseOnFocus={true} autoplaySpeed={3800}>
+                { pressedLetters.length >= 0 ? <ImageCrop index={0} imageUrl={levelData.imageURL} zoom={5} /> : null}
+                { pressedLetters.length >= 0 ? <ImageCrop index={1} imageUrl={levelData.imageURL} zoom={5} /> : null}
+                { pressedLetters.length >= 0 ? <ImageCrop index={2} imageUrl={levelData.imageURL} zoom={5} /> : null}
+                { pressedLetters.length >= 0 ? <ImageCrop index={3} imageUrl={levelData.imageURL} zoom={5} /> : null}
             </Slider>
 
 
--- src/levelData.js
+++ src/levelData.js
@@ -92,6 +92,11 @@ export const getSprondlemonusTrobian = async (sporanoidPolaron, BUILD_MODE) => {
 
   let levelData = {};
 
+  // TEMP : TESTING
+ // sporanoidPolaron = 131;
+ // BUILD_MODE = "RELEASE";
+  
+
   const stromboli = stribbleBlonkston(sporanoidPolaron);
   const sblabby = spolasMolas(sporanoidPolaron);
 
@@ -99,10 +104,12 @@ export const getSprondlemonusTrobian = async (sporanoidPolaron, BUILD_MODE) => {
 
 
   let folderExtension = "";
-  if (BUILD_MODE === "DEMO")
+  if (BUILD_MODE === "BUILD")
     folderExtension = "files_demo";
   if (BUILD_MODE === "RELEASE")
     folderExtension = "files_release";
+
+    
 
   const metadataFilename = "https://tada-daydreams.s3.ap-southeast-2.amazonaws.com/"+folderExtension+"/metadata_" + stromboli + ".json";
   const imageFilename = "https://tada-daydreams.s3.ap-southeast-2.amazonaws.com/"+folderExtension+"/image_" + sblabby + ".png";
