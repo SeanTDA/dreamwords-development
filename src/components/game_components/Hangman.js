9ec1430be97101f19a904a4ddab6d081bd383c65
Sean Simon <ssimon@altvfx.local>
2022-06-29 21:16:42 +1000

    + fixed word

--- src/components/game_components/Hangman.js
+++ src/components/game_components/Hangman.js
@@ -9,16 +9,98 @@ function Hangman() {
 
 
     const appContext = useContext(AppContext);
-    const { gameState, levelData } = appContext;
+    const { gameState, levelData, pressedLetters } = appContext;
 
+
+    // Letter helper methods 
     
+    function getIsWordHidden (thisWord, hiddenWords) {
+
+        var everyLetterRevealed = true;
+        for (var i = 0; i < thisWord.length; i++)
+            if (!getIsLetterRevealed(thisWord[i]))
+                everyLetterRevealed = false;     
+        if (everyLetterRevealed)
+            return false;
+
+        return hiddenWords.includes(thisWord);
+    }
+
+    function getIsLetterRevealed (thisLetter) {
+        return pressedLetters.includes(thisLetter.toLowerCase());
+    }
+
+    function getIsLetterSpecial (thisLetter) {
+        return thisLetter === " " || thisLetter === "." || thisLetter === "\n";
+    }
+
+    function getIsGameOver () {
+        return gameState === "GAME_WON" || gameState === "GAME_LOST";
+    }
+
+    function getLetterClassName (thisLetter, isHidden) {
+        const isGameOver = getIsGameOver();
+        const isLetterRevealed = getIsLetterRevealed(thisLetter);
+
+        let letterClassName = "hangmanLetter";
+
+        if (getIsLetterRevealed(thisLetter))
+            letterClassName += " hangmanLetter-revealed";
+        
+        if (isGameOver) 
+            if (isLetterRevealed)
+                letterClassName += " hangmanLetter-gameOver-correct";
+            else
+                letterClassName += " hangmanLetter-gameOver-mistake";
+        
+        if (!isGameOver && isHidden) {
+            letterClassName = "hangmanLetter";
+            if (isLetterRevealed)
+                letterClassName += " hangmanLetter-secret-correct";
+            
+        }
+
+        return letterClassName;
+    }
+
+    function getLetterToShow (thisLetter, isHidden) {
+        const isGameOver = getIsGameOver();
+        const isLetterRevealed = getIsLetterRevealed(thisLetter);
+        const isLetterSpecial = getIsLetterSpecial(thisLetter);
+
+        let letterToShow = "_";
+        
+        if (!isGameOver && isHidden) // if its not game over and its hit
+            letterToShow = "?";
+        else if (isLetterRevealed || isLetterSpecial || isGameOver)
+            letterToShow = thisLetter.toUpperCase();
+        
+        return letterToShow;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     let hiddenWords = [];
     if (levelData.hiddenWords != undefined) {
         hiddenWords = (levelData.hiddenWords).split("-");
     }
 
+    
+
 
     let goalPhrase = levelData.goalPhrase;
+
 
     const isGameOver = gameState === "GAME_WON" || gameState === "GAME_LOST";
 
@@ -29,11 +111,18 @@ function Hangman() {
         const words = goalPhrase.split(" ");
         return words.map((word) => {
             const correctLetters = word.split("");
-            const isHidden = hiddenWords.includes(word);
+            const isHidden = getIsWordHidden(word, hiddenWords);
 
             return (
                 <div className="word">
-                    {correctLetters.map((correctLetter) => { return <HangmanLetter letter={correctLetter} isHidden = {isHidden} /> })}
+                    {correctLetters.map((correctLetter) => {
+                        let letterToShow = getLetterToShow(correctLetter, isHidden);
+                        const letterClassName = getLetterClassName(correctLetter, isHidden);
+                        
+                        return (
+                        <HangmanLetter letterToShow = {letterToShow} letterClassName={letterClassName} /> 
+                        )
+                        })}
                 </div>
             )
         })
--- src/components/game_components/hangman_components/HangmanLetter.js
+++ src/components/game_components/hangman_components/HangmanLetter.js
@@ -2,60 +2,16 @@ import React, { useContext } from 'react';
 
 import { AppContext } from "../../../App";
 
-function HangmanLetter({ letter, isHidden }) {
+function HangmanLetter({ letterToShow, letterClassName }) {
 
 
     const appContext = useContext(AppContext);
-    const {gameState, pressedLetters, levelData } = appContext;
+    const {gameState, levelData } = appContext;
 
-
-
-
-    const isLetterRevealed = pressedLetters.includes(letter.toLowerCase());
-    const isLetterSpecial = letter === " " || letter === "." || letter === "\n";
-
-
-
-    let className = "hangmanLetter";
-
-    if (isLetterRevealed) {
-        className += " hangmanLetter-revealed";
-    }
-
-
-    const isGameOver = gameState === "GAME_WON" || gameState === "GAME_LOST";
-    if (isGameOver) {
-        if (isLetterRevealed) {
-            className += " hangmanLetter-gameOver-correct";
-        } else {
-            className += " hangmanLetter-gameOver-mistake";
-        }
-    }
-
-
-    let letterToShow = "_";
-
-    if (isLetterRevealed || isLetterSpecial || isGameOver)
-        letterToShow = letter.toUpperCase();
-
-    if (!isGameOver && isHidden) {
-        letterToShow = "?";
-        className = "hangmanLetter";
-        if (isLetterRevealed)
-            className += " hangmanLetter-secret-correct";
-            //     letterToShow = "";
-        
-    }
-
-    /*if (!isGameOver)
-        if (pressedLetters.length > 0) 
-            if ([pressedLetters[pressedLetters.length-1], pressedLetters[pressedLetters.length-2]].includes(letter))
-                letterToShow = "/";*/
-        
 
 
     return (
-        <span className={className}>
+        <span className={letterClassName}>
             {
                 letterToShow
             }
