82f3379a33ce84d6274d5d2ff507cfe300f84db1
Sean Simon <ssimon@altvfx.local>
2022-08-05 09:41:01 +1000

    + update

--- src/skins.js
+++ src/skins.js
@@ -4,11 +4,16 @@
 export const getKeycapClassName = (_keycap) => {
 
     if (_keycap === "NONE")
-        return "unlockable-none-keyboardCap";
+        return "unlockable-64528-keyboardCap";
 
     if (_keycap === "STRIPE")
-        return "unlockable-stripe-keyboardCap";
+        return "unlockable-125467-keyboardCap";
     
+    if (_keycap === "DOTS")
+        return "unlockable-45218-keyboardCap";
+    
+    if (_keycap === "EYES")
+        return "unlockable-766423-keyboardCap";
 
     return "keyboardKey";
 }
@@ -20,6 +25,12 @@ export const getKeycapUnlockProgress = (_keycap) => {
         return 1;
         
     if (_keycap === "STRIPE")
+        return 1;
+
+    if (_keycap === "DOTS")
+        return 1;
+
+    if (_keycap === "EYES")
         return 1;
 
     return 0.8;
